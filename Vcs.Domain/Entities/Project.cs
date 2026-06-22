017e559337e070e4e4a3d1b433f908affea7d344
Makvay <bojkov07@list.ru>
2026-06-22 23:04:35 +0800

    added entities

---
+++ Vcs.Domain/Entities/Project.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vcs.Domain.Entities
+{
+    internal class Project
+    {
+    }
+}
---
+++ Vcs.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vcs.Infrastructure.Data
+{
+    internal class AppDbContext
+    {
+    }
+}
